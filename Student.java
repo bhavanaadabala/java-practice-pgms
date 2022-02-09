b36384db9d61f5b0b548ae22f2f6705531ee11bb
bhavanaadabala <badabala@mondee.com>
2022-02-09 11:03:55 +0530

    hibernate task

---
+++ Student.java
@@ -0,0 +1,34 @@
+package com.mondee;
+
+public class Student {
+	private int sid;
+	private String sname;
+	private int smarks;
+	private String saddress;
+	
+	public int getSid() {
+		return sid;
+	}
+	public void setSid(int sid) {
+		this.sid = sid;
+	}
+	public int getSmarks() {
+		return smarks;
+	}
+	public void setSmarks(int smarks) {
+		this.smarks = smarks;
+	}
+	public String getSaddress() {
+		return saddress;
+	}
+	public void setSaddress(String saddress) {
+		this.saddress = saddress;
+	}
+	public String getSname() {
+		return sname;
+	}
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+	
+}
